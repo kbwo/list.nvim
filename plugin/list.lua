0e9a261e6fc64e753c2e41c3b905dabe8079a22a
kbwo <kbwo.owbk@gmail.com>
2025-11-04 21:57:38 +0900

    update

--- plugin/list.lua
+++ plugin/list.lua
@@ -3,23 +3,3 @@ if vim.g.loaded_list_nvim then
 end
 vim.g.loaded_list_nvim = true
 
--- Set up auto-continuation for markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    -- Insert mode: continue list on Enter
-    vim.keymap.set("i", "<CR>", function()
-      require("list").auto_continue_list()
-    end, { buffer = true, silent = true })
-
-    -- Normal mode: continue list on 'o' (open below)
-    vim.keymap.set("n", "o", function()
-      require("list").open_below_with_list()
-    end, { buffer = true, silent = true })
-
-    -- Normal mode: continue list on 'O' (open above)
-    vim.keymap.set("n", "O", function()
-      require("list").open_above_with_list()
-    end, { buffer = true, silent = true })
-  end,
-})
